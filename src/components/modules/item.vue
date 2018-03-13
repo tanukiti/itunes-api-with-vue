0aedfa11fb2f8db53e6235f62abeff46e912af5b
tanukiti <33892948+tanukiti@users.noreply.github.com>
2018-03-13 18:46:30 +0900

    vue

---
+++ src/components/modules/item.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{text}}</h1>
+</template>
+
+<script>
+export default {
+    props: ["text"],
+  data(){
+      return {
+          text: ""
+      }
+  }
+}
+</script>
---
+++ src/components/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+<section>
+  <h1>hello world</h1>
+  <item text="one"></item>
+
+</section>
+</template>
+
+<script>
+import Item from "../modules/item.vue"
+export default {
+    components: {
+        Item
+    },
+    created() {
+        console.log("created!")
+    },
+    mounted() {
+        console.log("mounted!")
+    }
+}
+</script>
